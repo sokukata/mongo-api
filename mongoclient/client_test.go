ac1b3ec56ecd5270ab35f2eafd030734daa19dab
lpatte <ludwig.patte@imagino.com>
2022-03-11 12:30:29 +0100

    add docker and compose + add test

--- mongoclient/client_test.go
+++ mongoclient/client_test.go
@@ -1,8 +1,10 @@
 package mongoclient
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http/httptest"
 	"os"
 	"testing"
@@ -35,8 +37,10 @@ func TestGetList(t *testing.T) {
 	w := httptest.NewRecorder()
 	getUsersList(w)
 	assert.Equal(t, w.Code, 200)
+
 	b, err := io.ReadAll(w.Body)
 	assert.NoError(t, err)
+
 	fmt.Println(string(b))
 }
 
@@ -45,8 +49,10 @@ func TestGetUser(t *testing.T) {
 	w := httptest.NewRecorder()
 	getUser(w, "KFPy6bbMWtPZ7CSqrQ7Qy3ilM2EYV8VzKPotC7SURNtSDhm1N2Q2POO94MMWuEyLjMPstBOAyQX0JBVsDQFgFrchfz42ObW3NpLE")
 	assert.Equal(t, w.Code, 200)
+
 	b, err := io.ReadAll(w.Body)
 	assert.NoError(t, err)
+
 	fmt.Println(string(b))
 }
 
@@ -59,9 +65,11 @@ func TestGeUpdate(t *testing.T) {
 	_ = os.MkdirAll(DIRECTORY, os.ModePerm)
 	updateUser(w, "KFPy6bbMWtPZ7CSqrQ7Qy3ilM2EYV8VzKPotC7SURNtSDhm1N2Q2POO94MMWuEyLjMPstBOAyQX0JBVsDQFgFrchfz42ObW3NpLE", mUser)
 	assert.Equal(t, w.Code, 200)
+
 	getUser(w, "KFPy6bbMWtPZ7CSqrQ7Qy3ilM2EYV8VzKPotC7SURNtSDhm1N2Q2POO94MMWuEyLjMPstBOAyQX0JBVsDQFgFrchfz42ObW3NpLE")
 	b, err := io.ReadAll(w.Body)
 	assert.NoError(t, err)
+
 	fmt.Println(string(b))
 }
 
@@ -70,10 +78,22 @@ func TestDelete(t *testing.T) {
 	w := httptest.NewRecorder()
 	deleteUser(w, "1qS9OI4YX8daKvHpwvhrUt6PVnG6MLQMemeFirBdqzEjwibcE1y1EZJELvXWi6w7hU9GwHMQ0RgVc3uWEOEJBbwolVD7rqIUgcwN")
 	assert.Equal(t, w.Code, 200)
+
 	getUser(w, "1qS9OI4YX8daKvHpwvhrUt6PVnG6MLQMemeFirBdqzEjwibcE1y1EZJELvXWi6w7hU9GwHMQ0RgVc3uWEOEJBbwolVD7rqIUgcwN")
 	assert.NotEqual(t, w.Code, 200)
 }
 
 func TestAdd(t *testing.T) {
-	//TODO
+	TestLogin(t)
+	w := httptest.NewRecorder()
+	jsonFile, err := os.Open("Dataset")
+	assert.NoError(t, err)
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	mUsers := make([]map[string]interface{}, 0)
+	err = json.Unmarshal(byteValue, &mUsers)
+	assert.NoError(t, err)
+
+	addUsers(w, mUsers)
+	assert.Equal(t, w.Code, 200)
 }
